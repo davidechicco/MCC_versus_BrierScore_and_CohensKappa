0b0976a05b9c9061cc87f642e74e6b95bf705ab7
davidechicco <davide.chicco@gmail.com>
2020-09-30 16:28:16 -0400

    added new files

--- bin/barplots_results.r
+++ bin/barplots_results.r
@@ -1,7 +1,7 @@
 setwd(".")
 options(stringsAsFactors = FALSE)
 cat("\014")
-set.seed(11)
+# set.seed(11)
 
 list.of.packages <- c("easypackages", "ggplot2")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -13,14 +13,27 @@ libraries(list.of.packages)
 #source("./confusion_matrix_rates.r")
 source("./utils.r")
 
-# 
-# normMCC	complBS
-# 0.079	0.586
-# 0.135	0.556
-# 0.069	0.500
-# 
-# 
-selected_predictions <- c(0.079, 	0.586, 0.135	, 0.556, 0.069,	0.500)
+# # # BS1, BS2, BS3
+# # 
+# # normMCC	complBS
+# # 0.079	0.586
+# # 0.135	0.556
+# # 0.069	0.500
+# # 
+# # 
+# selected_predictions <- c(0.079, 0.586, 0.135	, 0.556, 0.069, 0.500)
+
+# # # BS4, BS5, BS6
+# # 
+# # complBS	normMCC
+# # 0.581	0.080
+# # 0.558	0.116
+# # 0.524	0.085
+# selected_predictions <- c(0.080, 0.581,   0.116,  0.558, 0.085, 0.524)
+
+selected_predictions <- c(0.080, 0.581,   0.116,  0.558, 0.085, 0.524, 0.079, 0.586, 0.135, 0.556, 0.069, 0.500)
+
+
 test_title <- "use_cases"
 myPlotTitle <- "normMCC and complBS comparison"
 
@@ -29,8 +42,8 @@ myPlotTitle <- "normMCC and complBS comparison"
 
 
 results_dataframe <- data.frame(
-                cases=rep(c(" balanced", "neg imbal", "pos imbal"), each=2),
-                rates=rep(c("normMCC", "complBS"), 3),
+                cases=rep(c("balanced\nBS1", "neg imbal\nBS2", "pos imbal\nBS3", "balanced\nBS4", "neg imbal\nBS5", "pos imbal\nBS6"), each=2),
+                rates=rep(c("normMCC", "complBS"), 6),
  
                 values=selected_predictions
                 )
